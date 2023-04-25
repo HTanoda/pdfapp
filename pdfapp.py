ab80515a7ff4eed3610575269ad185452d457fca
HTanoda <131184857+HTanoda@users.noreply.github.com>
2023-04-25 22:52:13 +0900

    Add files via upload

--- pdfapp.py
+++ pdfapp.py
@@ -11,7 +11,8 @@ def read_pdf(file):
     text = ""
     file_buffer = BytesIO(file.getvalue())
     pdf = pdfreader.SimplePDFViewer(file_buffer)
-    for page in range(len(pdf.pages())):
+    num_pages = len(list(pdfreader.SimplePDFViewer(file_buffer).get_pages()))
+    for page in range(num_pages):
         pdf.navigate(page + 1)
         pdf.render()
         text += " ".join(pdf.canvas.strings)
@@ -23,11 +24,11 @@ def summarize_text(text):
     return summary
 
 def answer_question(question, context):
-    response = openai.Completion.create(engine="text-davinci-002", prompt=f"{question}\n{context}\nAnswer:", max_tokens=50)
+    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=f"{question}\n{context}\nAnswer:", max_tokens=50)
     answer = response.choices[0].text.strip()
     return answer
 
-st.title("PDF Summarizer and Q&A")
+st.title("PDF 要約 and Q&A")
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 if uploaded_file is not None:
     text = read_pdf(uploaded_file)
