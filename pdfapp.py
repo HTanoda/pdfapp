73d5440dd4edd526fda0b7c745266f1153e44979
HTanoda <131184857+HTanoda@users.noreply.github.com>
2023-04-26 00:22:47 +0900

    Add files via upload

--- pdfapp.py
+++ pdfapp.py
@@ -1,35 +1,70 @@
-import streamlit as st
+import os
 import openai
-import pdfplumber
-from io import BytesIO
-from transformers import pipeline
+import streamlit as st
+from PyPDF2 import PdfFileReader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import VectorDBQA, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain import PromptTemplate
 
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+os.environ["OPENAI_API_KEY"] = st.secrets.OpenAIAPI.openai_api_key
 
-def read_pdf(file):
-    text = ""
-    with pdfplumber.open(file) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+def load_pdf(file):
+    pdf_reader = PdfFileReader(file)
+    documents = [pdf_reader.getPage(i).extractText() for i in range(pdf_reader.getNumPages())]
+    return documents
 
-def summarize_text(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
-    return summary
+def process_documents(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(texts, embeddings)
+    return vectordb
 
-def answer_question(question, context):
-    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=f"{question}\n{context}\nAnswer:", max_tokens=50)
-    answer = response.choices[0].text.strip()
+def ask_question(vectordb, question):
+    qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=vectordb.as_retriever())
+    prompt = PromptTemplate(
+        input_variables=["question"],
+        template="あなたは親切なアシスタントです。下記の質問に日本語で回答してください。\n質問：{question}\n回答：",
+    )
+    answer = qa.ask(prompt.fill({"question": question}))
     return answer
 
-st.title("PDF 要約 and Q&A Powered by GPT")
-uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+def generate_summary(documents, language):
+    if language == 'Japanese':
+        summary_prompt = "この文書の要約を提供してください：\n\n{text}\n\n要約："
+    elif language == 'English':
+        summary_prompt = "Please provide a summary of this document:\n\n{text}\n\nSummary:"
+
+    summaries = []
+    for doc in documents:
+        summary = ask_question(vectordb, summary_prompt.format(text=doc))
+        summaries.append(summary)
+    return '\n'.join(summaries)
+
+st.title('PDF Summary and Q&A')
+uploaded_file = st.file_uploader('Upload a PDF file', type=['pdf'])
+
 if uploaded_file is not None:
-    text = read_pdf(uploaded_file)
-    summary = summarize_text(text)
-    st.write("Summary:", summary)
-    question = st.text_input("Ask a question based on the summary:")
+    with st.spinner('Processing PDF...'):
+        documents = load_pdf(uploaded_file)
+        vectordb = process_documents(documents)
+    st.success('PDF processed.')
+
+    summary_language = st.selectbox("Select summary language:", ['Japanese', 'English'])
+
+    with st.spinner('Generating summary...'):
+        summary = generate_summary(documents, summary_language)
+    st.subheader('Summary:')
+    st.write(summary)
+
+    question = st.text_input('Ask a question about the document:')
     if question:
-        answer = answer_question(question, summary)
-        st.write("Answer:", answer)
+        with st.spinner('Generating answer...'):
+            answer = ask_question(vectordb, question)
+        st.write(answer)
+
